ab4377f09450efbeddbc7f4d5c518ddaad05fdf5
Pete Dyer <pete.dyer@C02ZV0B6LVDN.local>
2022-02-07 11:37:41 +0000

    Add support for x509 certificsates.
    Added the with_psk flag into mbed_app.json. Set this to true to use PSK
    or false to use certificates.
    
    Modified device_config_serial_menu to optionally use certificate data
    built into the image.

---
+++ dev_credentials.c
@@ -0,0 +1,9 @@
+#ifndef __DEV_CREDENTIALS_H__
+#define __DEV_CREDENTIALS_H__
+
+#if !MBED_CONF_APP_WITH_PSK
+#error "Replace dev_credentials.c with your own developer cert."
+#endif
+
+
+#endif //__DEV_CREDENTIALS_H__
